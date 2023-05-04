09342abdacdb27c3b9c3be6e06b02c07e28c0ac3
bogdanmikh <bogdanmikh2007@gmail.com>
2023-05-04 18:45:46 +0300

    Fixed Bugs

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,5 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <AMSTL/Vector2.hpp>
+#include <iostream>
 
 int main() {
     GLFWwindow* window;
@@ -8,7 +10,6 @@ int main() {
     if (!glfwInit()) {
         return -1;
     }
-
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window) {
@@ -18,15 +19,27 @@ int main() {
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    amstl::vec2 point1;
+    point1 = {1, 1};
+    glClearColor(0.5, 0.3, 0.4, 1);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        glBegin(GL_TRIANGLES);
+        glVertex2f(point1.x, point1.y
+        );
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0, 0.5);
+        glEnd();
+
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
